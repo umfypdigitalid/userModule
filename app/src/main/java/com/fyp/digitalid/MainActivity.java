0ef63b11ed140b11887aa174791fe6d1644a8383
Loh Le Qing <leqingloh0915@gmail.com>
2021-05-16 22:44:04 +0800

    loginregister

--- app/src/main/java/com/fyp/digitalid/MainActivity.java
+++ app/src/main/java/com/fyp/digitalid/MainActivity.java
@@ -2,13 +2,24 @@ package com.fyp.digitalid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 //test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
+        findViewById(R.id.createBtn).setOnClickListener(this);
+*/
     }
+
+    /*@Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }*/
 }
